5cc67926870c75455b7a17c37bb2ba592a1647ee
Henrique Herbst Rosa <henriquehhr@gmail.com>
2022-02-08 23:45:43 -0300

    added server request to get chat messages and render them at startup

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ function closeSidebar(){
     }, 700);
 }
 
+/*
 function intializeServerConection(){
-    axios.post('http://mock-api.driven.com.br/api/v4/uol/participants', {
+    axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {
         name: 'dhfksdhfsdk'
       })
       .then(function (response) {
@@ -28,16 +29,60 @@ function intializeServerConection(){
       .catch(function (error) {
         console.log(error);
       });
+}*/
+
+function getChatMessages(){
+    let promisse = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    promisse.then(renderChatMessages);
 }
 
-function getMessages(){
-    let promisse = axios.get("http://mock-api.driven.com.br/api/v4/uol/messages");
-    promisse.then(renderMessages);
-}
-
-function renderMessages(messages){
+function renderChatMessages(messages){
     console.log(messages.data);
+    let main = document.querySelector("main");
+    for(let i = 0; i < messages.data.length; i++){
+        main.append(renderMessageHTMLFormat(messages.data[i]));
+    }
 }
 
-getMessages();
-intializeServerConection();
+function renderMessageHTMLFormat(message) {
+    let div = document.createElement("div");
+    div.classList.add("message");
+    let pMessage = document.createElement("p");
+    let spanMessageContent = document.createElement("span");
+    
+    let spanMessageTime = document.createElement("span");
+    spanMessageTime.classList.add("message__time");
+    spanMessageTime.innerHTML = "(" + message.time + ") ";
+    let strongFrom = document.createElement("strong");
+    strongFrom.innerHTML = message.from + " ";
+    let strongTo = document.createElement("strong");
+    strongTo.innerHTML = message.to + ": ";
+    let messageText = message.text;
+
+    switch(message.type){
+        case "status":{
+            spanMessageContent.append(strongFrom, messageText);
+            pMessage.append(spanMessageTime, spanMessageContent);
+            div.classList.add("message--status");
+            break;
+        }
+        case "message":{
+            spanMessageContent.append(strongFrom, "para ", strongTo, messageText);
+            pMessage.append(spanMessageTime, spanMessageContent);
+            break;
+        }
+        case "private_message":{
+            spanMessageContent.append(strongFrom, "reservadamente para ", strongTo, messageText);
+            pMessage.append(spanMessageTime, spanMessageContent);
+            div.classList.add("message--DM");
+            break;
+        }
+        default: console.log("deu erro");
+    }
+
+    div.append(pMessage);
+    return div;
+}
+
+getChatMessages();
+//intializeServerConection();
